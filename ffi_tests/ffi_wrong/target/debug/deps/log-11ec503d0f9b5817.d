30f668c3b7b03baf570bc149ac99007f1f0e8f53
Jennifer Switzer <jfs@mit.edu>
2020-02-18 17:47:10 -0500

    added out of bounds read example

--- ffi_tests/ffi_wrong/target/debug/deps/log-11ec503d0f9b5817.d
+++
@@ -1,9 +0,0 @@
-/home/jennifer/type_safe_ipc/ffi_tests/ffi_wrong/target/debug/deps/log-11ec503d0f9b5817.rmeta: /home/jennifer/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.8/src/lib.rs /home/jennifer/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.8/src/macros.rs /home/jennifer/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.8/src/serde.rs
-
-/home/jennifer/type_safe_ipc/ffi_tests/ffi_wrong/target/debug/deps/liblog-11ec503d0f9b5817.rlib: /home/jennifer/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.8/src/lib.rs /home/jennifer/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.8/src/macros.rs /home/jennifer/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.8/src/serde.rs
-
-/home/jennifer/type_safe_ipc/ffi_tests/ffi_wrong/target/debug/deps/log-11ec503d0f9b5817.d: /home/jennifer/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.8/src/lib.rs /home/jennifer/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.8/src/macros.rs /home/jennifer/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.8/src/serde.rs
-
-/home/jennifer/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.8/src/lib.rs:
-/home/jennifer/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.8/src/macros.rs:
-/home/jennifer/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.8/src/serde.rs:
--- ffi_tests/ffi_wrong/target/debug/libffi_test.d
+++
@@ -1,1 +0,0 @@
-/home/jennifer/type_safe_ipc/ffi_tests/ffi_wrong/target/debug/libffi_test.a: /home/jennifer/type_safe_ipc/ffi_tests/ffi_wrong/src/lib.rs
